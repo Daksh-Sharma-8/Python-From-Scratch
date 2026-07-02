edc360739b335556ba06209d52f8ff957a29527b
Daksh-Sharma-8 <daksh.sharma7817@gmail.com>
2026-07-02 17:51:14 +0530

    Add files via upload

---
+++ Practice Questions/P1.py
@@ -0,0 +1,22 @@
+#WAP to add 2 numbers through input and display the sum
+a = int(input("Enter First Number: "))
+b = int(input("Enter Second Number: "))
+sum = a + b
+print("Sum:", sum)
+
+#WAP to input side of a square and print its area
+a = int(input("Enter side of square: "))
+area = a * a
+print("Area of the square is:", area)
+
+#WAP to input 2 floating point numbers and print their average
+a = float(input("Enter First Number: "))
+b = float(input("Enter Second Number: "))
+average = (a + b) / 2
+print("Average:", average)
+
+#WAP to input 2 int numbers a and b. Print True if a is greater than or equal to b, else False.
+a = int(input("Enter First Number: "))
+b = int(input("Enter Second Number: "))
+print("It is", a >= b, "that", a, "is greater than or equal to", b)
+
